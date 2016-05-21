1048e7226f7bb9484f284e6fea7ff7c22ecd37eb
nuttanun pornpasittiphong <blacksheep_knack@hotmail.com>
2016-05-22 00:37:02 +0700

    test icon 3

--- resources/views/admin/course.blade.php
+++ resources/views/admin/course.blade.php
@@ -23,13 +23,13 @@
         <div class="box box-default">
           <div class="box-header with-border">
             <h3 class="box-title"><%course.name%></h3>
-            <button class="btn btn-default btn-flat"  ng-click="openEditPage(course.id)">
-              <i class="fa fa-align-left"></i>
-            </button>
-
-            <div class="box-tools pull-right">
-              <a href="{{ url('/admin/course/section')}}/<%course.id%>"><i class="class="fa fa-fw fa-cog""></i></a>
+            <button class="btn btn-default btn-flat">
+              <a href="{{ url('/admin/course/section')}}/<%course.id%>"><i class="class="fa fa-align-left""></i></a>
 			</button>
+            <div class="box-tools pull-right">
+                <button class="btn btn-default btn-flat"  ng-click="openEditPage(course.id)">
+                  <i class="fa fa-fw fa-cog"></i>
+                </button>
               <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i>
               </button>
             </div>
@@ -37,24 +37,13 @@
           </div>
           <!-- /.box-header -->
           <div class="box-body">
-          <div class "row">
-      		<div class="col-md-3">
+          
             <button ng-click="publish(course)" class="btn btn-success" ng-class="{'btn-danger':course.statusText=='unPublish','btn-success':course.statusText=='Publish'}"><%course.statusText%></button>
+            <div class="box-tools pull-right">
+            <button class="btn btn-danger" ng-click="deleteCourse(course.id)">
+              <i class="material-icons f18 mdl-color-text--red-400">clear</i>
+            </button>
             </div>
-            <div class="col-md-1"></div>
-            <div class="col-md-2">
-            
-			</div>
-            <div class="col-md-2">
-
-			</div>
-            <div class="col-md-3"></div>
-            <div class="col-md-1">
-<button class="btn btn-danger" ng-click="deleteCourse(course.id)">
-  <i class="material-icons f18 mdl-color-text--red-400">clear</i>
-</button>
-			</div>
-           </div>
           </div>
           <%course.about%>
           <!-- /.box-body -->
